51909ea21da6159df0d57842b64702ddb57fe076
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-14 14:10:11 -0700

    before merge

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './Application.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Homepage from "components/login/index"
-import Userpage from "components/Pages/user"
+import React, { Component } from "react";
+import "./Application.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Homepage from "components/login/index";
+import Userpage from "components/Pages/user";
 
 class Application extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <Router>
-      <div>
-      <Route exact path='/' component = {Homepage}/>
-      <Route exact path='/Userpage' component = {Userpage}/>
-      </div>
+        <div>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/Userpage" component={Userpage} />
+        </div>
       </Router>
-    )
+    );
   }
 }
 export default Application;
