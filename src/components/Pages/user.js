115c32f76d6389f319209bdf62c90c2ce015841a
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-15 11:24:23 -0700

    added 3 axios requests on volunteer page

--- src/components/Pages/user.js
+++ src/components/Pages/user.js
@@ -1,189 +1,205 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './style.scss';
-import Postit from './Postit';
-import UpdateForm from './UpdateForm';
-import Form from './form'; 
-import axios from 'axios'
+import "./style.scss";
+import Postit from "./Postit";
+import UpdateForm from "./UpdateForm";
+import Form from "./form";
+import axios from "axios";
 
 class Userpage extends Component {
-
   state = {
     postits: [
-    { title: 'grocery', description: 'mustard, tomato, fruit', colour: 'pink', key: '123hj$%656' },
-    { title: 'pet the cat', description: 'scratch behind the airs and sing to him', colour: 'blue' , key: '456k$%6lMy45' },
-  ],
-  toggleEditScreen: false,
-  postToEdit: undefined,
-}
+      {
+        title: "grocery",
+        description: "mustard, tomato, fruit",
+        colour: "pink",
+        key: "123hj$%656",
+      },
+      {
+        title: "pet the cat",
+        description: "scratch behind the airs and sing to him",
+        colour: "blue",
+        key: "456k$%6lMy45",
+      },
+    ],
+    toggleEditScreen: false,
+    postToEdit: undefined,
+  };
 
-//CREATE POST
-createPostit = (colour, title, description) => {
+  //CREATE POST
+  createPostit = (colour, title, description) => {
+    let postits = [...this.state.postits];
+    let newPost = {};
 
-  let postits = [...this.state.postits]
-  let newPost = {}
+    newPost.colour = colour;
+    newPost.title = title;
+    newPost.description = description;
+    newPost.key = title + Math.random();
+    postits.push(newPost);
+    this.setState({ postits: postits });
+  };
 
-  newPost.colour = colour
-  newPost.title = title
-  newPost.description = description
-  newPost.key = title + Math.random()
-  postits.push(newPost)
-  this.setState ({ postits: postits})
+  //Need to: Refractor & combine with onDragStart
+  //FINDS POST AND SAVES IT READT FOR EDIT, AND TOGGLES EDIT SCREEN
+  findPostToEdit = (key) => {
+    let newPostitsArray = [];
+    let postits = [...this.state.postits];
+    let postToEdit = {};
 
-} 
+    postits.forEach((post) => {
+      if (post.key !== key) {
+        newPostitsArray.push(post);
+      } else {
+        postToEdit = post;
+      }
+    });
+    this.setState({
+      postits: newPostitsArray,
+      postToEdit: postToEdit,
+      toggleEditScreen: true,
+    });
+  };
 
-//Need to: Refractor & combine with onDragStart
-//FINDS POST AND SAVES IT READT FOR EDIT, AND TOGGLES EDIT SCREEN
-findPostToEdit = (key) => {
-  let newPostitsArray = []
-  let postits = [...this.state.postits]
-  let postToEdit = {}
-  
-  postits.forEach((post) => {
-    if (post.key !== key) {
-     newPostitsArray.push(post)
-    } else {
-     postToEdit = post
-    }})
-  this.setState ({ postits: newPostitsArray, postToEdit: postToEdit, toggleEditScreen: true })
-}
+  //UPDATE POST
+  updatePostIt = (colour, title, description) => {
+    let postits = [...this.state.postits];
+    let postToEdit = this.state.postToEdit;
+    postToEdit.colour = colour;
+    postToEdit.title = title;
+    postToEdit.description = description;
+    postToEdit.key = title + Math.random();
+    postits.push(postToEdit);
+    this.setState({ postits: postits, toggleEditScreen: false });
+  };
 
-//UPDATE POST
-updatePostIt = (colour, title, description) => {
-  let postits = [...this.state.postits]
-  let postToEdit = this.state.postToEdit
-  postToEdit.colour = colour
-  postToEdit.title = title
-  postToEdit.description = description
-  postToEdit.key = title + Math.random()
-  postits.push(postToEdit)
-this.setState ({ postits: postits, toggleEditScreen: false})
-}
+  // Need to: Refractor & combine with findPostToEdit
+  // FINDS DRAGGED POST AND SAVES IT READY FOR DELETE
+  onDragStart = (key) => {
+    let newPostitsArray = [];
+    let postits = [...this.state.postits];
+    let postToEdit = {};
+    postits.forEach((post) => {
+      if (post.key === key) {
+        postToEdit = post;
+      }
+      this.setState({ postToEdit: postToEdit });
+    });
+  };
 
-// Need to: Refractor & combine with findPostToEdit
-// FINDS DRAGGED POST AND SAVES IT READY FOR DELETE
-onDragStart = (key) => {
-  let newPostitsArray = []
-  let postits = [...this.state.postits]
-  let postToEdit = {}
-  postits.forEach((post) => {
-    if (post.key === key) {
-     postToEdit = post
-    }
-  this.setState ({ postToEdit: postToEdit })
-})}
+  onDragOver = (e) => {
+    e.preventDefault();
+  };
 
-onDragOver = (e) => {
-  e.preventDefault()
-}
+  //DELETES POST OnDrop
+  onDrop = () => {
+    let key = this.state.postToEdit.key;
+    let newPostitsArray = [];
+    let postits = [...this.state.postits];
+    postits.forEach((post) => {
+      if (post.key !== key) {
+        newPostitsArray.push(post);
+      } else {
+      }
+    });
+    this.setState({ postits: newPostitsArray });
+  };
+  // componentDidMount() {
+  //   console.log('working')
+  // axios.post(`http://localhost:8001/api/services/create`,{
+  //  data
+  // }).then((response) => {
+  //   console.log(response)
 
-//DELETES POST OnDrop
-onDrop = () => {
-  let key = this.state.postToEdit.key
-  let newPostitsArray = []
-  let postits = [...this.state.postits]
-  postits.forEach((post) => {
-    if (post.key !== key) {
-     newPostitsArray.push(post)
-    } else {
-    }})
-  this.setState ({ postits: newPostitsArray })
-}
-// componentDidMount() {
-//   console.log('working')
-// axios.post(`http://localhost:8001/api/services/create`,{
-//  data
-// }).then((response) => {
-//   console.log(response)
-  
-// })
-// .catch((error) =>{
-//   console.log(error.message);
-// })
-// }
-componentDidMount() {
-  const data ={
-  user_id: 1,
-  category_id: 2, 
-  description: 'Hello world'
+  // })
+  // .catch((error) =>{
+  //   console.log(error.message);
+  // })
+  // }
+  componentDidMount() {
+    const data = {
+      user_id: 1,
+      category_id: 2,
+      description: "Hello world",
+    };
+    fetch("http://localhost:8001/api/services/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
-fetch('http://localhost:8001/api/services/create',{
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data)
-})
-.then((response) => response.json())
-.then((data) => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
- 
-}
-render() {
- 
- let postits = (
-       <div>
-        {this.state.postits.map((p) => {
-          return <Postit
-          colour={p.colour}
-          title={p.title}
-          description={p.description}
-          key={p.key}
-          onClick={(key) => this.findPostToEdit(p.key)}
-          onDragStart={(key) => this.onDragStart(p.key)}/>
-        })
-//reversing the array so the latest postit shows first
-        .reverse()} 
-      </div> 
+  render() {
+    let postits = (
+      <div>
+        {this.state.postits
+          .map((p) => {
+            return (
+              <Postit
+                colour={p.colour}
+                title={p.title}
+                description={p.description}
+                key={p.key}
+                onClick={(key) => this.findPostToEdit(p.key)}
+                onDragStart={(key) => this.onDragStart(p.key)}
+              />
+            );
+          })
+          //reversing the array so the latest postit shows first
+          .reverse()}
+      </div>
     );
 
+    //EDIT SCREEN VIEW
+    let editScreen;
+    if (this.state.toggleEditScreen) {
+      editScreen = (
+        <UpdateForm
+          colour={this.state.postToEdit.colour}
+          title={this.state.postToEdit.title}
+          description={this.state.postToEdit.description}
+          key={this.state.postToEdit.key}
+          updatePostIt={this.updatePostIt}
+        />
+      );
+    } else {
+    }
 
-//EDIT SCREEN VIEW
-let editScreen
-   if (this.state.toggleEditScreen) {
-    editScreen = 
-        <UpdateForm 
-        colour={this.state.postToEdit.colour}
-        title={this.state.postToEdit.title}
-        description={this.state.postToEdit.description}
-        key={this.state.postToEdit.key} 
-        updatePostIt={this.updatePostIt}/>
-     } else {
-   }
-
-//THE RETURN BLOCK
+    //THE RETURN BLOCK
     return (
       <div>
-      <div className="banner">
-      <header className="logo">
-      <h1>@VolunHero</h1>
-      </header>
-      </div>
-      <div className="Appl">
-        <header className="App-header">
-          
-          <h1>Post your Request</h1>
-          <h2>VolunHero are at your service</h2>
-     
-      <div className="wrapper">  
-        <Form createPostit={this.createPostit}/>
-         <div className="trash-can"
-         onDrop={() => this.onDrop()}
-         onDragOver={(e) => this.onDragOver(e)}>
-         <h2 className="align"> 🗑️ </h2>
-         <h4> drag & Drop</h4>
+        <div className="banner">
+          <header className="logo">
+            <h1>@VolunHero</h1>
+          </header>
         </div>
-         </div>
-        </header>
-         <ul>
-          {postits}
-        </ul>
+        <div className="Appl">
+          <header className="App-header">
+            <h1>Post your Request</h1>
+            <h2>VolunHero are at your service</h2>
+
+            <div className="wrapper">
+              <Form createPostit={this.createPostit} />
+              <div
+                className="trash-can"
+                onDrop={() => this.onDrop()}
+                onDragOver={(e) => this.onDragOver(e)}
+              >
+                <h2 className="align"> 🗑️ </h2>
+                <h4> drag & Drop</h4>
+              </div>
+            </div>
+          </header>
+          <ul>{postits}</ul>
           {editScreen}
-      </div>
+        </div>
       </div>
     );
   }
--- src/components/admin/volunteer.js
+++ src/components/admin/volunteer.js
@@ -1,61 +1,101 @@
-import React, {Component} from 'react';
-import Modal from 'react-modal';
-import axios from 'axios';
+import React, { Component } from "react";
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
+import axios from "axios";
+
 export default class volunteerpage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            services: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      services: [],
+      users: {},
+      categories: {},
+    };
+  }
 
-    componentDidMount() {
-        let self = this;
-        console.log('reloaded')
-        axios.get('http://localhost:8001/api/services/',
-        // axios.get('http://localhost:8001/api/users/'    
-        ).then(function(response) {
-            if (response.status >= 400) {
-                throw new Error("Bad response from server");
-            }
-            console.log(response.data)
-            return response.data.services;
-        }).then(function(data) {
-            self.setState({services: data});
-        }).catch(err => {
-        console.log('caught it!',err);
-        })
-    }
-    
-
-    render() {
-        return (
-        <div className="container"> 
-            <div className="panel panel-default p50 uth-panel">
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>user_id</th>
-                            <th>services</th>
-                            <th>category_id</th>
-                            <th>Action</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.services.map(member =>
-                        <tr key={member.id}>
-                        <td>{member.user_id} </td>
-                        <td>{member.description}</td>
-                        <td>{member.category_id}</td>
-                        <td><a>Accept</a>|<a>Complete</a></td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
-                </div>
-                </div>
-                    
-        )
-            }
+  componentDidMount() {
+    let self = this;
+    console.log("reloaded");
+    Promise.all([
+      fetch(`http://localhost:8001/api/services/`),
+      fetch(`http://localhost:8001/api/users/`),
+      fetch(`http://localhost:8001/api/categories/`),
+    ])
+      .then(function (response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
         }
+        console.log(response.data);
+        return response.data.services;
+      })
+      .then(function (data) {
+        self.setState({ services: data });
+      })
+      .catch((err) => {
+        console.log("caught it!", err);
+      });
+  }
+
+  // useEffect(() => {
+  // let self = this;
+  //   Promise.all([
+  //     axios.get("http://localhost:8001/api/services/"),
+  //     axios.get("http://localhost:8001/api/users/"),
+  //     axios.get("http://localhost:8001/api/categories/"),
+  //   ]).then((all) => {
+  //     this.state((state) => ({
+  //       ...state,
+  //       services: all[0].data,
+  //       users: all[1].data,
+  //       categories: all[2].data,
+  //     }));
+  //   });
+  // }, []);
+  //     if (response.status >= 400) {
+  //       throw new Error("Bad response from server");
+  //     }
+  //     console.log(response.data);
+  //     return response.data.services;
+  //   })
+  //   .then(function (data) {
+  //     self.setState({ services: data });
+  //   })
+  //   .catch((err) => {
+  //     console.log("caught it!", err);
+
+  render() {
+    return (
+      <div className="container">
+        <div className="banner">
+          <header className="logo">
+            <h1>@VolunHero</h1>
+          </header>
+        </div>
+        <div className="panel panel-default p50 uth-panel">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>user_id</th>
+                <th>services</th>
+                <th>category_id</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.services.map((member) => (
+                <tr key={member.id}>
+                  <td>{member.user_id} </td>
+                  <td>{member.description}</td>
+                  <td>{member.category_id}</td>
+                  <td>
+                    <a>Accept</a>|<a>Complete</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
