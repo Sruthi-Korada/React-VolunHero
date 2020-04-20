300244e79b0fca8ce2c0537089ac27888d3a91b2
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-20 13:44:32 -0700

    mergeing

--- src/components/Pages/user.js
+++ src/components/Pages/user.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import { Redirect } from "react-router-dom";
 import { Navbar, Row, Col, Nav } from "react-bootstrap";
-=======
-import{Redirect} from 'react-router-dom'
-import {Navbar, Row, Col, Nav} from "react-bootstrap";
-import LogoutIcon from '../admin/logout.png'
->>>>>>> 73853329f7937f44ac023e7fab7bd1b68114c88a
+import LogoutIcon from "../admin/logout.png";
 import "./style.scss";
 import Postit from "./Postit";
 import UpdateForm from "./UpdateForm";
@@ -108,32 +103,6 @@ class Userpage extends Component {
   };
   // Need to: Refractor & combine with findPostToEdit
   // FINDS DRAGGED POST AND SAVES IT READY FOR DELETE
-  onDragStart = (key) => {
-    let postits = [...this.state.postits];
-    let postToEdit = {};
-    postits.forEach((post) => {
-      if (post.key === key) {
-        postToEdit = post;
-      }
-      this.setState({ postToEdit: postToEdit });
-    });
-  };
-  onDragOver = (e) => {
-    e.preventDefault();
-  };
-  //DELETES POST OnDrop
-  onDrop = () => {
-    let key = this.state.postToEdit.key;
-    let newPostitsArray = [];
-    let postits = [...this.state.postits];
-    postits.forEach((post) => {
-      if (post.key !== key) {
-        newPostitsArray.push(post);
-      } else {
-      }
-    });
-    this.setState({ postits: newPostitsArray });
-  };
   render() {
     if (this.state.redirectTo == true) {
       return <Redirect to="/" />;
@@ -159,198 +128,10 @@ class Userpage extends Component {
                 );
               return undefined;
             })
-<<<<<<< HEAD
             //reversing the array so the latest postit shows first
             .reverse()}
       </React.Fragment>
     );
-    //EDIT SCREEN VIEW
-    let editScreen;
-    if (this.state.toggleEditScreen) {
-      editScreen = (
-        <UpdateForm
-          colour={this.state.postToEdit.colour}
-          title={this.state.postToEdit.title}
-          description={this.state.postToEdit.description}
-          key={this.state.postToEdit.key}
-          show={this.state.toggleEditScreen}
-          updatePostIt={this.updatePostIt}
-          onClose={this.toggleEditScreen}
-        />
-      );
-=======
-            .then(([res1]) => {
-                this.setState({ postits: res1.services });
-            })
-            .catch((err) => {
-                console.log("caught it!", err);
-            });
-    }
-    onDelete = (cardId) => {
-        const data = JSON.stringify({id: cardId})
-        fetch('http://localhost:8001/api/services/delete', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',       // receive json
-                'Content-Type': 'application/json'
-            },
-            body: data
-        })
-            .then(res => res.json())
-            .then(data => this.fechServices())
-    };
-    //CREATE POST
-    createPostit = (description, category_id, is_completed) => {
-        let postits = [...this.state.postits];
-        let newPost = {};
-
-        //newPost.colour = colour;
-        newPost.category_id = category_id;
-        newPost.description = description;
-        newPost.is_completed = is_completed;
-        newPost.key = 1 + Math.random();
-        postits.push(newPost);
-
-        this.fechServices();
-
-        // this.setState({ postits: postits });
-    };
-
-    //Need to: Refractor & combine with onDragStart
-    //FINDS POST AND SAVES IT READT FOR EDIT, AND TOGGLES EDIT SCREEN
-    findPostToEdit = (key) => {
-        let newPostitsArray = [];
-        let postits = [...this.state.postits];
-        let postToEdit = {};
-
-        postits.forEach((post) => {
-            if (post.key !== key) {
-                newPostitsArray.push(post);
-            } else {
-                postToEdit = post;
-                // newPostitsArray.push(post);
-            }
-        
-        });
-
-        this.setState({
-            postits: newPostitsArray,
-            postToEdit: postToEdit,
-            toggleEditScreen: true,
-        });
-    };
-
-    //UPDATE POST
-    updatePostIt = (colour, title, description) => {
-
-        let postits = [...this.state.postits];
-        let postToEdit = this.state.postToEdit;
-        postToEdit.colour = colour;
-        postToEdit.title = title;
-        postToEdit.description = description;
-        postToEdit.key = title + Math.random();
-        postits.push(postToEdit);
-        // TODO: make a post call
-
-        //http://localhost:8001/api/services/create
-        Axios.post(`http://localhost:8001/api/services/create`, postToEdit)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-
-        this.setState({ postits: postits, toggleEditScreen: false });
-    };
-
-    // TOGGLE SHOW EDIT MODAL
-    toggleEditScreen = () => {
-        this.setState({ toggleEditScreen: false });
-    };
-
-    // Need to: Refractor & combine with findPostToEdit
-    // FINDS DRAGGED POST AND SAVES IT READY FOR DELETE
-  
-
- 
-  
-    render() {
-        if(this.state.redirectTo == true){
-            return (<Redirect to="/" />)
-        }
-        let postits = (
-            <React.Fragment>
-                {this.state.postits && this.state.postits
-                    .map((p) => {
-                        if (p.user_id === 3)
-                            return (
-                                <Postit
-                                    cardId={ p.id}
-                                    onDelete = {this.onDelete}
-                                    category_id={p.category_id}
-                                    description={p.description}
-                                    volunteer_user_id = {p.volunteer_user_id}
-                                    key={p.key}
-                                    is_completed={p.is_completed}
-                                    onClick={(key) => this.findPostToEdit(p.key)}
-                                    onDragStart={(key) => this.onDragStart(p.key)}
-                                />
-                            );
-
-                        return undefined;
-                    })
-                    //reversing the array so the latest postit shows first
-                    .reverse()}
-            </React.Fragment>
-        );
-
-        
-        //THE RETURN BLOCK
-        return (
-            <React.Fragment>
-                <Navbar bg="primary" variant="dark" className="app__bar">
-                    <img className="app__logo" src={Logo}/>
-                    <Nav className="mr-auto"></Nav>
-                    <Nav>
-                        <button className="delete">Emergency Contact: +1 555 555 5555</button>
-                        <button className="delete" onClick={()=>{
-                            this.setState({redirectTo: true})
-                        }}><img src={LogoutIcon} alt="" />Logout</button>
-                    </Nav>
-                </Navbar>
-                <div className="hero__banner">
-                    <div className="form__container">
-                        <div className="flex__form">
-                                <div className="interest__form">
-                                    <RequestForm createPostit={this.createPostit} />
-                                </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="quote__banner">
-                    <div className="info__container">
-                        <div className="info__wrapper">
-                            <div className="quote__left">
-                                <h3 className="banner__heading">Helping Hands Are Better Than Praying Hands</h3>
-                                <span className="banner__span">
-                                    You Have 2 hands. One to help yourself, One to help others
-                                </span>
-                            </div>
-                            <div className="quote__right">
-                                <a href="#" className="help__cta">HELP US</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="posts__container">
-                    {postits}
-                </div>
-                <Footer />
-            </React.Fragment>
-        );
->>>>>>> 73853329f7937f44ac023e7fab7bd1b68114c88a
-    }
     //THE RETURN BLOCK
     return (
       <React.Fragment>
@@ -359,7 +140,7 @@ class Userpage extends Component {
           <Nav className="mr-auto"></Nav>
           <Nav>
             <button className="delete">
-              Emergency Contact: +1 508 445 9343
+              Emergency Contact: +1 555 555 5555
             </button>
             <button
               className="delete"
@@ -367,7 +148,8 @@ class Userpage extends Component {
                 this.setState({ redirectTo: true });
               }}
             >
-              SignOff
+              <img src={LogoutIcon} alt="" />
+              Logout
             </button>
           </Nav>
         </Navbar>
--- src/components/admin/volunteer.js
+++ src/components/admin/volunteer.js
@@ -21,7 +21,6 @@ export default class volunteerpage extends Component {
       isDataFetched: false,
       redirectTo: false,
     };
-<<<<<<< HEAD
   }
   componentDidMount() {
     NProgress.start();
@@ -57,87 +56,14 @@ export default class volunteerpage extends Component {
   render() {
     if (this.state.redirectTo == true) {
       return <Redirect to="/" />;
-=======
-
-
-    render() {
-        if(this.state.redirectTo == true){
-            return (<Redirect to="/" />)
-        }
-        return (
-            <div>
-                <Navbar bg="primary" variant="dark" className="app__bar">
-                    <img className="app__logo" src={Logo}/>
-                    <Link to="/vounteerservice">Accepted Services</Link> <span className="menu__divider">|</span>
-                    <Link to="/completedservice">Completed Services</Link>
-                    <Nav className="mr-auto"></Nav>
-                    <Nav>
-                       <div className="delete__container">
-                        <button className="delete delete__align" onClick={()=>{
-                            this.setState({redirectTo: true})
-                        }}><img src={LogoutIcon} alt="" />Logout</button>
-                        </div>
-                    </Nav>
-                </Navbar>
-                <div style={{ padding: 20 }}>
-                    <Table  className="services__table" responsive>
-                        <thead>
-                        <tr>
-                            <th>S.No.</th>
-                            <th>Name</th>
-                            <th className="services__header">Services</th>
-                            <th>Category</th>
-                            <th>Address</th>
-                            <th>City</th>
-                            <th>Country</th>
-                            <th>Phone Number</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.services &&
-                        this.state.services.map((member, index) => (
-                            <tr key={member.id}>
-                                <td>{index + 1}</td>
-                                <td>{member.name}</td>
-                                <td className="services__header">{member.description}</td>
-                                <td>{member.category}</td>
-                                <td>{member.address}</td>
-                                <td>{member.city}</td>
-                                <td>{member.country}</td>
-                                <td>{member.phone}</td>
-                                <td>
-                                    <Button
-                                    className="button_admin_cta"
-                                        variant="primary"
-                                        size="sm"
-                                        onClick={ (e)=>{
-                                            this.onAccept(e, member)
-                                        }}
-                                        value={'1'}
-                                    >
-                                        Accept
-                                    </Button>
-
-
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-                </div>
-                <Footer />
-            </div>
-        );
->>>>>>> 73853329f7937f44ac023e7fab7bd1b68114c88a
     }
     return (
       <div>
         <Navbar bg="primary" variant="dark" className="app__bar">
           <img className="app__logo" src={Logo} />
-          <Link to="/vounteerservice">Accepted Service</Link>{" "}
+          <Link to="/vounteerservice">Accepted Services</Link>{" "}
           <span className="menu__divider">|</span>
-          <Link to="/completedservice">Completed Service</Link>
+          <Link to="/completedservice">Completed Services</Link>
           <Nav className="mr-auto"></Nav>
           <Nav>
             <div className="delete__container">
@@ -148,7 +74,7 @@ export default class volunteerpage extends Component {
                 }}
               >
                 <img src={LogoutIcon} alt="" />
-                SignOff
+                Logout
               </button>
             </div>
           </Nav>
